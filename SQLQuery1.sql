22991143a54c67dbb873e395da828718ecaacafa
Ömer Faruk Fidan <fidanomerfaruk01@gmail.com>
2023-08-03 17:17:01 +0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+
+--select CityID,CityName, LatestRecordedPopulation from Application.Cities where LatestRecordedPopulation > 0 order by LatestRecordedPopulation 
+
+
+
+
+select COUNT(*) from Sales.Invoices 
+select COUNT(*) from Application.DeliveryMethods 
+select COUNT(*) from Sales.Customers
